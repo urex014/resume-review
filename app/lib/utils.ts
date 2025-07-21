8239c0c837e7caf7695da4aabf6d1f88c88b2112
Amarachukwu <amarachukwuonuohabars007@gmail.com>
2025-07-22 00:12:50 +0100

    feat: cloud storge, pdf to image conversion key-value storage, ai feedback

---
+++ app/lib/utils.ts
@@ -0,0 +1,3 @@
+export const generateuuid = () => {
+    return crypto.randomUUID();
+}
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,12 +1,74 @@
 import React, { type FormEvent } from 'react'
 import Navbar from '~/components/Navbar'
 import {useState} from 'react'
-import FileUploader from '~/components/fileUploader';
+import FileUploader from '~/components/FileUploader';
+import { usePuterStore } from '~/lib/puter';
+import { useNavigate } from 'react-router';
+import { generateuuid } from '~/lib/utils';
+import { convertPdfToImage } from '~/lib/pdf2image';
+// import { stringify } from 'querystring';
+import { prepareInstructions } from '../../constants';
 
 const upload = () => {
-    const[isProcessing, seIsProcessing]=useState(false);
-    const[statusText, setSatatusText]=useState('')
+    const {auth, isLoading, fs, ai, kv} = usePuterStore();
+    const navigate = useNavigate();
+    const[isProcessing, setIsProcessing]=useState(false);
+    const[statusText, setStatusText]=useState('')
     const [file, setFile] = useState<File | null>(null);
+
+
+interface AnalyzeInput{
+    companyName:string,
+    jobTitle:string,
+    jobDescription:string,
+    file:File
+}
+
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file }: { companyName: string, jobTitle: string, jobDescription: string, file: File  }) => {
+        setIsProcessing(true);
+
+        setStatusText('Uploading the file...');
+        const uploadedFile = await fs.upload([file]);
+        if(!uploadedFile) return setStatusText('Error: Failed to upload file');
+
+        setStatusText('Converting to image...');
+        const imageFile = await convertPdfToImage(file);
+        if(!imageFile.file) return setStatusText('Error: Failed to convert PDF to image');
+
+        setStatusText('Uploading the image...');
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if(!uploadedImage) return setStatusText('Error: Failed to upload image');
+
+        setStatusText('Preparing data...');
+        const uuid = generateuuid();
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName, jobTitle, jobDescription,
+            feedback: '',
+        }
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+        setStatusText('Analyzing...');
+
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({ jobTitle, jobDescription, AIResponseFormat: 'json' })
+        );
+        if (!feedback) return setStatusText('Error: Failed to analyze resume');
+
+        const feedbackText = typeof feedback.message.content === 'string'
+            ? feedback.message.content
+            : feedback.message.content[0].text;
+
+        data.feedback = JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setStatusText('Analysis complete, redirecting...');
+        console.log(data);
+        navigate(`/resume/${uuid}`);
+    }
+
 
 
     const handleSubmit=(e:FormEvent<HTMLFormElement>)=>{
@@ -14,10 +76,11 @@ const upload = () => {
         const form = e.currentTarget.closest('form');
         if(!form) return;
         const formData = new FormData(form);
-        const companyName = formData.get('company-name');
-        const jobTitle = formData.get('job-title');
-        const jobDescription = formData.get('job-description')
-        console.log(companyName)
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string; 
+        const jobDescription = formData.get('job-description') as string;
+        if(!file) return;
+        handleAnalyze({companyName, jobTitle, jobDescription, file});
 }
 
 const handleFileSelect=(file:File|null)=>{
