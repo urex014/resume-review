20192caed468496a71e741356c9e6ddd65674285
Amarachukwu <amarachukwuonuohabars007@gmail.com>
2025-07-22 01:19:37 +0100

    resume feeback page

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,6 @@ export default [
     index("routes/home.tsx"),
     route('/auth', "routes/auth.tsx"),
     route('/upload', "routes/upload.tsx"),
+    route('/resume/:id', "routes/resume.tsx"),
 
 ] satisfies RouteConfig;
